c12fe19ade082328255bf86fb4683a4728e65330
ZecroZhang <68031710+ZecroZhang@users.noreply.github.com>
2024-07-22 19:03:59 -0400

    feat: add fill_rect function

--- tetris.asm
+++ tetris.asm
@@ -74,3 +74,42 @@ game_loop:
 
     #5. Go back to 1
     b game_loop
+
+# arguments: a0 to a3, v0 and t0, t1
+# a0 = start x
+# a1 = start y
+# a2 = width
+# a3 = height (modified)
+# v0 = return address (pc)
+# t0 = screen location 
+# t1 = colour
+# uses registers t2, t3, t4
+fill_rect: 
+    # t2 = pointer of y
+    # t2 = 4(start_x + 32 start_y) + display_offset
+    # t2 = 4(a0 + 32a1) + t0
+    sll $t2, $a1, 5 # a1 * 32
+    add $t2, $t2, $a0 # a0 + 32a1
+    sll $t2, $t2, 2 # 4(a0 + 32a1)
+    add $t2, $t2, $t0 
+
+outer_fill_rect_loop:
+    # t3 = pointer of x
+    move $t3, $t2
+    # t4 = copy of width (so we can decrease it in a loop)
+    move $t4, $a2
+
+inner_fill_rect_loop:
+    sw $t1, 0($t3) # draw pixel
+    addi $t3, $t3, 4 # move x pointer
+    addi $a2, $a2, -1 # decrease width
+    bgtz $a2, inner_fill_rect_loop
+
+# outer loop end
+    addi $t2, $t2, 128 # increment the y pointer to the next line(this is the start x position)
+    addi $a3, $a3, -1 # decrease height
+    move $a2, $t4 # restore the width
+    bgtz $a3, outer_fill_rect_loop
+
+    # link back to the program
+    jr $v0
